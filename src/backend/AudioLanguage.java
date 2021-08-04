6e8841e361269cfce0870d2ad1f6345304ca8b3d
Bright Osuagwu <bo.osuagwu@gmail.com>
2021-08-04 12:44:02 +0100

    Added another package

---
+++ src/backend/AudioLanguage.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backend;
+
+/**
+ *
+ * @author redux23
+ */
+public enum AudioLanguage {
+    ENGLISH,
+    FRENCH,
+    GERMAN,
+    RUSSIAN,
+    ITALIAN,
+    SPANISH,
+    CANTONESE
+}
